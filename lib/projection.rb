398c345633c1675888d26d66eb60c322fc0a085c
yar <yar@laptop>
2009-06-11 03:11:40 +0400

    better naming

--- lib/projection.rb
+++ lib/projection.rb
@@ -1,10 +1,10 @@
 class Projection
-  def initialize(forecast, acc_method)
-    @forecast = forecast
+  def initialize(lookup_like_obj, acc_method)
+    @lookup_like_obj = lookup_like_obj
     @acc_method = acc_method
   end
   
   def per(*args)
-    @forecast.send(@acc_method, *args)
+    @lookup_like_obj.send(@acc_method, *args)
   end
 end
